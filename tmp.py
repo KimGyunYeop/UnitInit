40828f8230bab611771f3ae4bc7643f31ef409f1
YooEunseok <sunnycloud0144@gmail.com>
2024-03-18 16:40:33 +0900

    tmp

--- tmp.py
+++ tmp.py
@@ -6,11 +6,14 @@ runs = api.runs(path="isnlp_lab/unit_init_glue")
 
 a = set()
 for r in runs:
-    if r.state == "crashed" or r.state == "failed":
-        continue
+    # if r.state == "crashed" or r.state == "failed":
+    #     continue
     
-    print(r.name)
-    print(r.state)
-    a.add(r.state)
-    # break
-print(a)
+    if "noact_" in r.name:
+        if "no_act" in r.name or "elu" in r.name:
+            print(r.name)
+            print(str(r.name).replace("noact_",""))
+            
+            r.name = str(r.name).replace("noact_","")
+            r.update()
+    
